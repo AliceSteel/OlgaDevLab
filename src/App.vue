d9b3fdf267eff69dcef378557f91d6b368616a6b
AliceSteel <alicedevlab@gmail.com>
2026-09-18 13:54:54 +0200

    added analytic

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
   <main class="bg-gray-100 min-h-screen">
     <RouterView />
   </main>
+  <Analytics />
 </template>
 
 <script setup lang="ts">
 import AppNav from './components/AppNav.vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <style scoped></style>
